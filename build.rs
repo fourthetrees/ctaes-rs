ff4a5bccc6a4f55e188962286f59c6281fe33f39
forrest <fspmarshall@gmail.com>
2017-04-26 17:46:25 -1000

    added dependency folder & modified build.rs to auto-download ctaes

--- build.rs
+++ build.rs
@@ -1,8 +1,20 @@
 extern crate gcc;
+use std::process::Command;
+use std::path::Path;
 
 fn main() {
+    if !Path::new("dep/ctaes/.git").exists() {
+        let cmd = Command::new("git")
+            .args(&["submodule", "update", "--init"])
+            .status()
+            .unwrap();
+        if !cmd.success() {
+            panic!("failed to initialize git submodule: `ctaes`")
+        }
+    }
+
     // compile `ctaes` to a static lib.
     gcc::Config::new()
-        .file("ctaes/ctaes.c")
+        .file("dep/ctaes/ctaes.c")
         .compile("libctaes.a");
 }
